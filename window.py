e6bff7764f52d661ac60b0f5d6050a7dc26ff907
BlueMarth <hubert.lcr@gmail.com>
2023-10-12 16:22:36 +0800

    Commencement

---
+++ window.py
@@ -0,0 +1,27 @@
+import pygame
+from sys import exit
+
+# initiate everything for pygame
+pygame.init()
+# display surface
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('insert title')
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100,200))
+test_surface.fill('red')
+
+running = True
+
+while running:
+    # detect for any user input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    # draw all our elements
+    screen.blit(test_surface, (200,100)) #blit: block image transfer i.e. put one surface on top of another
+
+    # update everything
+    pygame.display.update()
+    clock.tick(60)
